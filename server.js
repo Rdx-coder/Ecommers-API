a2f1e8eb5f59e64fce54d96a65dc3e298d7bc4b9
rdx_coder <rahul.dangi.sait@gmail.com>
2023-06-27 20:03:09 +0530

    change in json

--- server.js
+++ server.js
@@ -3,16 +3,24 @@ const mongoose = require('mongoose');
 const productRoutes = require('./routes/productRoutes');
 require('dotenv').config();
 const app = express();
-const MONGODB_URI = 'mongodb://127.0.0.1/ecommerce';
+const db = require('./config/key').MongoURI; // Requiring MongoDB URI from configuration file
+
+// const MONGODB_URI = 'mongodb://127.0.0.1/ecommerce';
 
 // Connect to MongoDB
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('Failed to connect to MongoDB', error);
-  });
+// mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => {
+//     console.log('Connected to MongoDB');
+//   })
+//   .catch((error) => {
+//     console.error('Failed to connect to MongoDB', error);
+//   });
+
+
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }) // Connecting to MongoDB
+    .then(() => console.log("Successfully connected to MongoDB")) // On successful connection, log the success message
+    .catch(err => console.log(err)); // On error, log the error message
+
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
